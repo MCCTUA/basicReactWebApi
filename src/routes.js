64c2941cab29c9950d388f1e625a65a2b0e263fb
MCCTUA <mcctua@gmail.com>
2024-04-14 09:00:46 +0700

    page not found

--- src/routes.js
+++ src/routes.js
@@ -2,6 +2,7 @@ import { Routes, Route } from "react-router-dom";
 import Login from "./pages/login/Login";
 import Register from "./pages/register/Register";
 import Forgotpassword from "./pages/forgotpassword/Forgotpassword";
+import PageNotFound from "./pages/pagenotfound/PageNotFound";
 
 const MyRoutes = () => {
     return (
@@ -15,6 +16,8 @@ const MyRoutes = () => {
 
             {/* Backend */}
             {/* <Route path="/" element={<Dashboard />} /> */}
+
+            <Route path='*' element={<PageNotFound />} />
         </ Routes >
     )
 }
